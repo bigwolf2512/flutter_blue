dc3c9e71b5e4d68cb2b557ab82c1d17809858b8c
Tu Dev <tunguyen.hcmus@gmail.com>
2023-01-17 00:03:36 +0700

    first init

--- .history/example/lib/main_20230116234437.dart
+++ .history/example/lib/main_20230116234437.dart
@@ -54,7 +54,7 @@ class BluetoothOffScreen extends StatelessWidget {
               'Bluetooth Adapter is ${state != null ? state.toString().substring(15) : 'not available'}.',
               style: Theme.of(context)
                   .primaryTextTheme
-                  .subtitle1
+                  .subhead
                   ?.copyWith(color: Colors.white),
             ),
           ],
